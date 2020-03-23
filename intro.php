23812411625fdb439a9d24407e709bd4faf76676
aravind0612 <leelavathi@fixnix.co>
2020-03-23 16:12:29 +0530

    meta tag added

--- intro.php
+++ intro.php
@@ -2,14 +2,16 @@
 <html lang="en" >
  <head><!--begin::Base Path (base relative path for assets of this page) -->
 <base href="/corona/"><!--end::Base Path -->
-        <meta charset="utf-8"/>
-
-        <title>Corona</title>
-        <meta name='viewport' content='width=device-width, initial-scale=1'>
+ <meta charset="utf-8"/>
+ <title>Corona Speak | Coronavirus Updated News</title>
+<meta name='viewport' content='width=device-width, initial-scale=1'>
 <script src='https://kit.fontawesome.com/a076d05399.js'></script>
-        <meta name="description" content="Base form control examples">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-          <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<meta name="keywords" content="Coronavirus,Coronaspeak,Coronavirus live update,Coronavirus live status,Corona
+Coronavirus,Coronaspeak,Coronavirus live update,Coronavirus live status,Corona,Corona news,Corona Live Statistics,corana in India">
+<meta name="description" content="Corona speak provide the Live Statistics by country wise and updated information and news for coronavirus">
+<link rel="canonical" href="https://www.coronaspeak.com/corona/intro.php" />
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
